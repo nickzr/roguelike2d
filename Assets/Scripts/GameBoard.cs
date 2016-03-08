074957aa5fb1aa464561d3e089adbceb49eca6f9
Nikolaj Rasmussen <nikolaj_rasmussen@hotmail.dk>
2016-03-08 13:07:21 +0100

    Initial commit

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameBoard : MonoBehaviour {
+	public int rows;
+	public int columns;
+
+	public GameObject[] groundTiles;
+	public GameObject[] wallTiles;
+
+	private Transform boardHolder;
+
+	// Use this for initialization
+	void Awake () {
+		boardSetup ();
+	}
+
+	void boardSetup(){
+		boardHolder = new GameObject ("Board").transform;
+
+		for (int i = -1; i <= rows; i++) {
+			for (int j = -1; j <= columns; j++) {
+				GameObject toInstantiate;
+			}
+		}
+	}
+}
